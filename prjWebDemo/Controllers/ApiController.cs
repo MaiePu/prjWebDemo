1f64035a18956f417800842d1102a9afaeccd543
MaiePu <vickexe@gmail.com>
2023-01-06 17:29:55 +0800

    加入專案檔案。

---
+++ prjWebDemo/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace prjWebDemo.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            //System.Threading.Thread.Sleep(5000);
+
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "vick";
+            }
+
+            return Content($"Hello , {name}","text/plain",Encoding.UTF8);
+        }
+    }
+}
---
+++ prjWebDemo/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjWebDemo.Models;
+using System.Diagnostics;
+
+namespace prjWebDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DemoContext _conetxt;
+
+        public HomeController(ILogger<HomeController> logger, DemoContext conetxt)
+        {
+            _logger = logger;
+            _conetxt = conetxt;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            var members = _conetxt.Members;
+            return View(members);
+        }
+
+        public IActionResult FirstVick()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
